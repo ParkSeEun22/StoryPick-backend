414b40a8137b46a2f129e3dc08076be8a90fc367
ParkSeEun22 <sso789432@gmail.com>
2024-05-07 20:02:48 +0900

    first commit

---
+++ src/main/java/com/example/storypickbackend/StoryPickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.storypickbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryPickBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryPickBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storypickbackend/StoryPickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storypickbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryPickBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
